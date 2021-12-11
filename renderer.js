ac65d96cab5e77f6cfc56574779d3d850703900f
thedukeofcool <maybinullo@icloud.com>
2021-12-11 15:06:52 -0500

    a0.1-dev1-3d-3
    now fixed, and faster

--- renderer.js
+++ renderer.js
@@ -1,4 +1,3 @@
-
 var vertexShaderText = 
 [
 'precision mediump float;',
@@ -27,11 +26,12 @@ var fragmentShaderText =
 '}'
 ].join('\n');
 
+var gl;
+
 var runRender = function() {
-     
 
     var canvas = document.getElementById('canvas');
-    var gl = canvas.getContext('webgl');
+    gl = canvas.getContext('webgl');
 
     if (!gl) {
         gl = canvas.getContext('experemental-webgl');
@@ -44,6 +44,11 @@ var runRender = function() {
     gl.clearColor(0.5, 0.5, 0.5, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.frontFace(gl.CCW);
+    gl.cullFace(gl.BACK);
+
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
@@ -77,10 +82,10 @@ var runRender = function() {
     var boxVertices =
         [ // X, Y, Z           R, G, B
             // Top
-            -1.0, 1.0, -1.0, 0.5, 0.5, 0.5,
-            -1.0, 1.0, 1.0, 0.5, 0.5, 0.5,
-            1.0, 1.0, 1.0, 0.5, 0.5, 0.5,
-            1.0, 1.0, -1.0, 0.5, 0.5, 0.5,
+            -1.0, 1.0, -1.0, 0.0, 0.0, 0.0,
+            -1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
+            1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
+            1.0, 1.0, -1.0, 0.0, 0.0, 0.0,
 
             // Left
             -1.0, 1.0, 1.0, 0.75, 0.25, 0.5,
@@ -181,7 +186,7 @@ var runRender = function() {
     var projMatrix = new Float32Array(16);
 
     glMatrix.mat4.identity(worldMatrix);
-    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
     glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.clientHeight, 0.1, 1000.0);
 
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
@@ -190,12 +195,17 @@ var runRender = function() {
 
     gl.useProgram(program);
 
+    var xRotationMatrix = new Float32Array(16);
+    var yRotationMatrix = new Float32Array(16);
+
     var angle = 0;
-    var indentityMatrix = new Float32Array(16);
-    glMatrix.mat4.identity(indentityMatrix);
+    var identityMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(identityMatrix);
     var loop = function() {
         angle = performance.now() / 1000 / 6 * 2 * Math.PI;
-        glMatrix.mat4.rotate(worldMatrix, indentityMatrix, angle, [0, 1, 0]);
+        glMatrix.mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
+        glMatrix.mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
+        glMatrix.mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
         gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
         gl.clearColor(0.5, 0.5, 0.5, 1.0);
